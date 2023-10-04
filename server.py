4559af2dfc909a85e307747b1933b256588b3842
Abel Woldemichael <abelllselam@gmail.com>
2023-10-04 19:10:40 -0400

    new-app_double_charging_fruit_store

--- server.py
+++ server.py
@@ -1,18 +1,34 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
 
-@app.route('/')         
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+
+@app.route("/checkout", methods=["POST"])
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    form_info = request.form
+    print("This form request is printing from the checkout route", form_info)
 
-@app.route('/fruits')         
+    sum = (
+        int(form_info["strawberry"])
+        + int(form_info["raspberry"])
+        + int(form_info["apple"])
+    )
+
+    print(f"Charging {form_info['first_name']} for {sum} fruits")
+
+    return render_template("checkout.html", form_info=form_info)
+
+
+@app.route("/fruits")
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
